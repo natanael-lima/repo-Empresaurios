af9358695e1bdae65f1cdee52ba3f1b5969b5d78
Natanael <Natanael@Natanael>
2021-05-06 17:13:22 -0600

    Creacion de clase model

--- Lima_Natanael_3907_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
+++ Lima_Natanael_3907_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
@@ -9,6 +9,54 @@ public class Compra {
     private int cantidad;
     private double total;
     
+    
+   public Compra() {
+	   
+   }
+    
+    
+	public Compra(int id, Producto producto, int cantidad, double total) {
+		super();
+		this.id = id;
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.total = total;
+	}
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + ", total=" + total + "]";
+	}
+    
+	
+    
+    
 	
 }
