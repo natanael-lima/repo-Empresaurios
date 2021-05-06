73b5b9de3598831991f84e11a1ddc2ce3d89ad32
Natanael <Natanael@Natanael>
2021-05-06 17:01:59 -0600

    Creacion de atributos del model

---
+++ Lima_Natanael_3907_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.tp4.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Compra {
+@Autowired
+	private int id;
+	private Producto producto;
+    private int cantidad;
+    private double total;
+    
+	
+	
+}
